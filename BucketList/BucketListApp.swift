c5852a53d6c090607cf431392159aafb94f347f6
Andres <andresr774@gmail.com>
2022-03-29 12:32:14 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Andres camilo Raigoza misas on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
